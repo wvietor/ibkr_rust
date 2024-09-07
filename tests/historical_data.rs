d72e1e7a2bd6a79dc3fd3261e2a1b56d3cc0f7e0
William Vietor <will.vietor@gmail.com>
2024-09-06 20:53:35 -0400

    added new test for historical data

---
+++ tests/historical_data.rs
@@ -0,0 +1,47 @@
+use ibapi::prelude::*;
+use std::future::Future;
+use tokio_util::time::FutureExt;
+
+struct ChannelWrapper {
+    tx: tokio::sync::mpsc::Sender<(i64, Vec<Bar>)>,
+}
+
+impl Wrapper for ChannelWrapper {
+    fn historical_bars(&mut self, req_id: i64, bars: Vec<Bar>) -> impl Future + Send {
+        async move {
+            let _ = self.tx.send((req_id, bars)).await;
+        }
+    }
+}
+
+#[tokio::test]
+async fn spy_bars() -> Result<(), Box<dyn std::error::Error>> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let mut client = Builder::from_config_file(Mode::Paper, Host::Gateway, &Some("config.toml"))?
+        .connect(8)
+        .await?
+        .disaggregated(ChannelWrapper { tx })
+        .await;
+
+    let spy = contract::new::<Stock>(&mut client, "BBG000BDTBL9".parse()?).await?;
+    let id = client
+        .req_historical_bar(
+            &spy,
+            historical_bar::EndDateTime::Present,
+            historical_bar::Duration::Week(1),
+            historical_bar::Size::Day,
+            historical_bar::Trades,
+            false,
+        )
+        .await?;
+    if let Some(msg) = rx
+        .recv()
+        .timeout(std::time::Duration::from_secs(15))
+        .await?
+    {
+        assert_eq!(id, msg.0);
+        println!("{:?}", msg.1.first());
+    }
+
+    Ok(())
+}
